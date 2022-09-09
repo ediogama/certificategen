794e4eea95d4565da7bf44220570d039acc2abc4
Edio Gama <ediogama@hotmail.com>
2022-09-08 21:13:41 -0300

    add generate pdf

--- serverless.ts
+++ serverless.ts
@@ -46,7 +46,7 @@ const serverlessConfiguration: AWS = {
       bundle: true,
       minify: false,
       sourcemap: true,
-      exclude: ['aws-sdk'],
+      exclude: ['aws-sdk', 'chrome-aws-lambda'],
       target: 'node14',
       define: { 'require.resolve': undefined },
       platform: 'node',
